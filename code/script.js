45af54c3686f13dd5e506e0fb4bafe82115b8488
AnaClaraprismas <an4claracouto@gmail.com>
2024-08-07 09:49:31 -0300

    cauculo de imc finalizado

--- code/script.js
+++ code/script.js
@@ -10,7 +10,7 @@ formulario.onsubmit = (event) => {
 
   const altura = parseFloat(document.querySelector("#altura").value);
   const peso = parseFloat(document.querySelector("#peso").value);
-
+  const imc = calculoImc(altura, peso)
   // Faça condicionais verificando os dados fornecidos
 
   // Verificar se os dados inseridos são válidos
@@ -18,4 +18,20 @@ formulario.onsubmit = (event) => {
   // Se está abaixo do peso
   // Se está com o peso ideal
   // Se está acima do peso
-};
+  if (imc >= 18.5 && imc < 25) {
+    resultadoTexto.textContent = `seu imc é: ${imc.toFixed(2)}. Você está no seu peso ideal!`
+  }
+  else if (imc >= 25 && imc < 30) {
+    resultadoTexto.textContent = `seu imc é: ${imc.toFixed(2)}. Você está acima do ideal!`
+  }
+  else if (imc >=30 && imc <35) {
+    resultadoTexto.textContent = `seu imc é: ${imc.toFixed(2)}. Você está obeso classe I!`
+  }
+  else if (imc >= 35 && imc < 40) {
+    resultadoTexto.textContent = `seu imc é: ${imc.toFixed(2)}. Você está obeso classe II!`
+  }
+  else if (imc > 40) {
+
+    resultadoTexto.textContent = `seu imc é: ${imc.toFixed(2)}. Você está classe III!`
+  }
+}
